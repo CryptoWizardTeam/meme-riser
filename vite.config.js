88c42b2ff7ee2fd2a92ed57f36eaa09df29f6c4c
CryptoWizardTeam <markojokic588@gmail.com>
2025-07-13 20:40:05 +0000

    update-vite.config.js file to fix netlify issue

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self'; style-src 'self';"
+    }
+  }
 })
